56ffef0456b27c401145641046098fe8d4a08533
vakfiend <vakfiend@proton.me>
2024-09-21 11:01:58 +0000

    this is my scripts

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var click=0; 
+var c = document.getElementById("click");
+function hi(){
+    click+=1;
+    c.innerHTML = click;
+}
+function subtract(){
+    click+=-1;
+    c.innerHTML = click;
+}
+function reset(){
+    click=0;
+    c.innerHTML = click;
+}
